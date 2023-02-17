c2fd766758723059b6ca02933723abbe9ded8a86
Tamim <tamimarif03@gmail.com>
2023-02-17 19:13:29 +0500

    make bug

---
+++ Lecture2/For30.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var n, a, b float64
+	fmt.Scan(&n, &a, &b)
+	d := math.Abs(a - b)
+	each := d / n
+	for i := 0; i < int(n); i++ {
+		fmt.Println(1 - math.Sin(a + (each*i)))
+	}
+	// Still got a error
+}
