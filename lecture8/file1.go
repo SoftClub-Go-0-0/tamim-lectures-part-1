339ae1af7ef6c5c6b0c1c8fbce1d973e70b73b83
Tamim <tamimarif03@gmail.com>
2023-03-14 18:49:02 +0500

    done

---
+++ lecture8/file1.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var filename string
+	fmt.Scan(&filename)
+
+	f, err := os.Create(filename)
+	if err != nil {
+		//log.Fatal(err)
+		fmt.Println(err)
+		fmt.Println(false)
+		return
+	}
+	fmt.Println(true)
+	f.Close()
+	os.Remove(f.Name())
+}
